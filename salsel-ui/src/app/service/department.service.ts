fecdd068539844c5997f4d89866f743b9236df1c
Usman Digitaldev <usmanaslamm138@gmail.com>
2023-11-30 20:28:59 +0500

    updated

--- salsel-ui/src/app/service/department.service.ts
+++ salsel-ui/src/app/service/department.service.ts
@@ -1,13 +1,12 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class DepartmentService {
-
   constructor(private http: HttpClient) {}
 
   editTicketMode = new BehaviorSubject(false);
@@ -15,8 +14,7 @@ export class DepartmentService {
 
   // url = "http://localhost:8080/api/";
 
-  url = environment.URL + "/department";
-
+  url = environment.URL + "department";
 
   // Create Ticket
 
