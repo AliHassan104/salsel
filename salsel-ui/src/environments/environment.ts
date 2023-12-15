ff983d82af87fcc09820e13af8c05b13bccb0e89
Muhammad Tabish Rashid <muhammadtabish05@gmail.com>
2023-12-15 21:56:38 +0500

    Changed to prod

--- salsel-ui/src/environments/environment.ts
+++ salsel-ui/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
 
-  URL : "http://localhost:8080/api/",
+  // URL : "http://localhost:8080/api/",
+  URL : "https://api.salselexpress.com/api/"
 };
 
 /*
